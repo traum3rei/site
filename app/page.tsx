9095eaeeb868c130bc8668a3fd4a9ff7ee239b37
Traumerei <atakan.sent@gmail.com>
2024-04-27 00:35:54 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,75 +1,17 @@
-'use client'
-
 import Image from "next/image";
-import React, {useEffect, useState} from 'react';
-import { MenuList, MenuListItem, Separator, styleReset } from 'react95';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import { Frame } from 'react95';
-
-
-/* Pick a theme of your choice */
-import original from 'react95/dist/themes/original';
-
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-
-
-const GlobalStyles = createGlobalStyle`
-  ${styleReset}
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  body, input, select, textarea {
-    font-family: 'ms_sans_serif';
-  }
-`;
-const getWindowDimensions = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-};
-const useWindowDimensions = () => {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-  window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  return windowDimensions;
-};
+import React from 'react';
 
 export default function Home() {
-  const {height, width} = useWindowDimensions();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
-    <ThemeProvider theme={original}>
-      
-      <Frame
-          variant='field'
-          style={{
-            height: height-0.05*height,
-            width: width-0.05*width 
-          }}
-        >
-          Hello.
-        </Frame>
-    </ThemeProvider>
+    
+    <div>
+          <h1>Hello. My name is Atakan and I like tech.</h1>
+          <h2>Testing</h2>
+    </div>
+    <div>Hello</div>
+
     </main>
   );
 }
